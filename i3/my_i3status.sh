92a7da10198b6f9fce27ffda14cd49b17eee113e
Martin Barreau <martin.barreau@gmail.com>
2021-07-10 22:36:31 +0200

    (refacto) i3: scratchpads, xss-lock & i3status

---
+++ i3/my_i3status.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+i3status "$@" | (read line; echo "$line"; read line ; echo "$line" ; read line ; echo "$line" ; while true
+do
+	read line
+	id=$(xprop -root | awk '/_NET_ACTIVE_WINDOW\(WINDOW\)/{print $NF}')
+	if [ x$id != x ]; then
+		name=$(xprop -id $id | awk '/_NET_WM_NAME/{$1=$2="";print}' | cut -d'"' -f2)
+		name=${name//\\/\\\\}
+		name=${name//\"/\\\"}
+		dat="[{\"color\":\"#ECEFF4\",\"name\":\"title\",\"full_text\":\"$name\"},"
+		echo "${line/[/$dat}" || exit 1
+	else
+		echo "$line" || exit 1
+	fi
+done)
